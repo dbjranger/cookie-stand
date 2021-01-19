476a662d93ceeb3fcf55ab1e7c6c792c12bade6e
don <bandy@gmail.com>
2021-01-18 17:03:59 -0800

    Added Min/Max customers and Average Cookies per location title, Created the layout for Cookies purchased per hour by location

---
+++ app.js
@@ -0,0 +1,62 @@
+
+//Stores the min/max hourly customers, and the average cookies per customer, in object properties
+var Seattle = {
+  minHourlyCustomers: null,
+  maxHourlyCustomers: null,
+  averageCookiesPerHour: null,
+  results: []
+
+}
+
+var Tokyo = {
+  minHourlyCustomers: null,
+  maxHourlyCustomers: null,
+  averageCookiesPerHour: null,
+  results: []
+
+}
+
+var Dubai = {
+  minHourlyCustomers: null,
+  maxHourlyCustomers: null,
+  averageCookiesPerHour: null,
+  results: []
+
+}
+
+var Paris = {
+  minHourlyCustomers: null,
+  maxHourlyCustomers: null,
+  averageCookiesPerHour: null,
+  results: []
+
+}
+
+var Lima = {
+  minHourlyCustomers: null,
+  maxHourlyCustomers: null,
+  averageCookiesPerHour: null,
+  results: []
+
+}
+
+//Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+var customersPerHour = Math.floor(Math.random() * 50)
+var cookiesPerHour = Math.floor(Math.random() * 100)
+
+//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+      
+
+
+//Store the results for each location in a separate array… perhaps as a property of the object representing that location
+      //results: []
+
+
+
+//Display the values of each array as unordered lists in the browser
+      //<ul>
+          //<li></li>
+      //</ul>
+
+
+//Calculating the sum of these hourly totals; your output for each location should look like this:
