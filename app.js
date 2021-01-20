8499e9ad464f748d4be43dd4a80f188f6f855874
don <bandy@gmail.com>
2021-01-19 22:22:43 -0800

    added data to tables

--- app.js
+++ app.js
@@ -1,29 +1,128 @@
-//Stores the min/max hourly customers, and the average cookies per customer, in object properties
-var allLocations = []; 
+var storeTimes = [ " ", "6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am",
+  "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "Daily Location Total"]
 
-function Location(minHourlyCustomers, maxHourlyCustomers, averageCookiesPerCustomer, locationName) {
-      this.totalCustomersDaily = 0;
-      this.minHourlyCustomers = minHourlyCustomers;
-      this.maxHourlyCustomers = maxHourlyCustomers;
-      this.averageCookiesPerCustomer = averageCookiesPerCustomer;
-      this.locationName = locationName;
-    
-      this.cookiesHourly = [];
-      //this.cookieTotal = null;
-      allLocations.push(this)
+// Should "Total" be the last variable here to complete the table structure?
+  var cityLocations = [ "Seattle", "Tokyo", "Dubai", "Paris", "Lima" ]
+
+
+
+function createHeader() {
+  for (var i = 0; i < storeTimes.length; i++) {
+    //Add headers to table
+    var tdParent = document.getElementById("Times");
+    var td = document.createElement("td");
+    var string = storeTimes[i];
+    td.innerHTML = `${string}`;
+    tdParent.appendChild(td);
+  }
+}
+createHeader();
+
+function createCityList() {
+//Add cities to the left navigation of the table
+  for (var i = 0; i < cityLocations.length; i++) {
+    var tdParent = document.getElementById(cityLocations[i]);
+    var td = document.createElement("td")
+    var string = cityLocations[i];
+    td.innerHTML = `${string}`  
+    tdParent.appendChild(td);
+    }
+} 
+createCityList();
+
+
+var cookiesHr = [];
+for (var i = 0; i < 14; i++) {
+  cookiesHr.push(Math.floor(Math.random() * 100))
+}
+
+var totalCustomerCount = Math.floor(Math.random() * 100)
+
+var minimumCustomerCount = (Math.floor(Math.random() * 10));
+
+var maximumCustomerCount = (Math.floor(Math.random() * 75));
+
+var allLocations = [];
+
+
+function Location(locationName, totalCustomersDaily, minHourlyCustomers, maxHourlyCustomers, cookiesHourly) {
+  this.locationName = locationName;
+  this.totalCustomersDaily = totalCustomersDaily;
+  this.minHourlyCustomers = minHourlyCustomers;
+  this.maxHourlyCustomers = maxHourlyCustomers;
+  this.cookiesHourly = cookiesHourly;
+  this.allData = this
 }
 
 
-var seattle = new Location(5, 10, 3.2, "Seattle");
-var tokyo = new Location(1, 2, 5.5, "Tokyo");
-var dubai = new Location(3, 8, 2.3, "Dubai");
-var paris = new Location(6, 4, 5.2, "Paris");
-var lima = new Location(7, 7, 7.7, "Lima"); 
+
+// totalCustomer, minimum, and maximum customer count are all the same number. 
+
+  
+  var seattle = new Location("Seattle", totalCustomerCount, minimumCustomerCount, maximumCustomerCount, cookiesHr)
+  var tokyo = new Location("Tokyo", totalCustomerCount, minimumCustomerCount, maximumCustomerCount, cookiesHr);
+  var dubai = new Location("Dubai", totalCustomerCount, minimumCustomerCount, maximumCustomerCount, cookiesHr);
+  var paris = new Location("Paris", totalCustomerCount, minimumCustomerCount, maximumCustomerCount, cookiesHr);
+  var lima = new Location("Lima", totalCustomerCount, minimumCustomerCount, maximumCustomerCount, cookiesHr);
 
 
-seattle.minHourlyCustomers = Math.min.apply(Math, this.totalCustomersDaily);
+  console.log(seattle.allData);
 
-console.log(seattle.minHourCustomers)
+
+//console.log(seattle);
+//console.log(tokyo);
+//console.log(dubai);
+//console.log(paris);
+//console.log(lima);
+
+
+// var cityLocations = [ "Seattle", "Tokyo", "Dubai", "Paris", "Lima" ]
+
+
+// This for loop needs to loop through each cityLocation and enter their cookies per hour into the table. 
+for (var i = 0; i < cityLocations.length; i++) {
+  var tdParent = document.getElementById(cityLocations[i]);
+  var td = document.createElement("td");
+  var string = cityLocations[i];
+  td.innerHTML = `${string}`;
+  //tdParent.appendChild(td);
+  for (var j = 0; j < cookiesHr.length; j++) {
+    var td2 = document.createElement("td");
+    var string2 = cookiesHr[j]
+    td2.innerHTML = `${string2}`
+    tdParent.appendChild(td2)
+
+  }
+}
+
+
+//Find the totals for each hour
+
+
+//Find the totals for each city Location
+
+
+
+// Issues:  1) Same totalCustomer, minimum, and maximum customer count are all the same number.
+//          2) Can't access the hourlycookies per location in the for loop
+//          3) How to calculate totals for hour
+//          4) How to calculate totals for each city Location
+
+
+
+
+
+
+
+
+
+
+/*
+//var minimumCustomerCount = Math.min.apply(Math, totalCustomerCount);
+
+//Stores the min/max hourly customers, and the average cookies per customer, in object properties
+var allLocations = []; 
+var totalCustomersDaily = Math.floor(Math.random() * 100);
 
 
 
@@ -41,54 +140,21 @@ function averageCookiesPerCustomer() {
 //  
 //}
 
-function maxHourCustomers() {
-  Math.max.apply(Math, this.totalCustomersDaily);
-}
-
-var cookiesHourly = [];
 
 
-for (var i = 0; i < 14; i++) {
-  cookiesHourly.push(Math.floor(Math.random() * 100))
-}
+var  = document.createElement("li");
+var num = Math.floor(Math.random() * 100);
+var string = `${timeAM}am: ${num} cookies`;
+li.innerHTML = `${string}`;
+ul.appendChild(li);
 
 
-var storeTimes = [ " ", "6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am",
-  "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "Daily Location Total"]
-
-var cityLocations = [ "Seattle", "Tokyo", "Dubai", "Paris", "Lima" ]
-
-
-
-function createHeader() {
-  for (var i = 0; i < storeTimes.length; i++) {
-    //Add headers to table
-    var tdParent = document.getElementById("Times");
-    var td = document.createElement("td");
-    var string = storeTimes[i];
-    td.innerHTML = `${string}`;
-    tdParent.appendChild(td);
-  }
-}
-createHeader();
-
-function createCityList() {
-  //Add cities to the left navigation of the table
-    for (var i = 0; i < cityLocations.length; i++) {
-      var tdParent = document.getElementById(cityLocations[i]);
-      var td = document.createElement("td")
-      var string = cityLocations[i];
-      td.innerHTML = `${string}`  
-      tdParent.appendChild(td);
-    }
-} 
-createCityList();
+var li = document.createElement("li");
+var num = Math.floor(Math.random() * 100);
+var string = `${timePM}pm: ${num} cookies`;
+li.innerHTML = `${string}`;
+ul.appendChild(li);
   
-
-
-
-
-
 
 function createList() { 
     var li = document.createElement("li");
@@ -123,6 +189,12 @@ for (var i = 0; i < 13; i++) {
 };
 
 
+
+
+
+function maxHourCustomers() {
+  Math.max.apply(Math, this.totalCustomersDaily);
+}
 
 
 
@@ -208,26 +280,12 @@ var customersPerHour = Math.floor(Math.random() * 50);
 var cookiesPerHour = Math.floor(Math.random() * 100);
 
 //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
 
 var timeAM = 6;
 var timePM = 1;
 
-function createList() {
-  if (timeAM < 13) {  
-    var li = document.createElement("li");
-    var num = Math.floor(Math.random() * 100);
-    var string = `${timeAM}am: ${num} cookies`;
-    li.innerHTML = `${string}`;
-    ul.appendChild(li);
-  } else {
-    var li = document.createElement("li");
-    var num = Math.floor(Math.random() * 100);
-    var string = `${timePM}pm: ${num} cookies`;
-    li.innerHTML = `${string}`;
-    ul.appendChild(li);
-  }
-}
+
 
 
 
